2f85d75f12c2d728f78f0410a190b92ad6a456de
junseong-kwon <rnjswnstjd13@gmail.com>
2024-06-27 16:04:43 +0900

    ;

--- app/src/main/java/bitcamp/project1/Test01.java
+++ app/src/main/java/bitcamp/project1/Test01.java
@@ -1,11 +1,20 @@
 package bitcamp.project1;
 
 public class Test01 {
+  void test1() {
+    System.out.println("dmdkdk");
+  }
+}
 
   public static void main(String[] args) {
     String a = "와우 테스트다";
     System.out.println(a);
+
+    static void m1() {
+      System.out.println("오호라!!!");
+    }
+
   }
 
 
-}
+
